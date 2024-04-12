122f7e372e6b6ba5fbd0551f0116776f2464f17d
Ashutosh Kumar <ashutosh.k@braneenterprises.com>
2024-04-12 23:06:12 +0530

    Implements login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,28 @@
 import User from '../models/userModel.js';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+
+// create jwt token
+const createToken = (_id) =>{
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
+
 
 // login user
 export const login = async(req, res) => {
-    res.json({msg : 'login user'});
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.login(email, password);
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token});
+
+    } catch(error){
+        res.status(400).json({error: error.message});
+    }
 }
 
 // signup user
@@ -11,13 +30,14 @@ export const login = async(req, res) => {
 //to do so we can handle it in signup controller or mongoose provide some
 // static method to do so
 export const signup = async function (req, res) {
-
     const {email, password} = req.body;
 
     try{
-        console.log("email, pass", email, password);
         const user = await User.signUp(email, password);
-        res.status(200).json({email, user});
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token});
 
     } catch(error){
         res.status(400).json({error: error.message});
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -52,4 +52,26 @@ userSchema.statics.signUp = async function (email, password) {
     }
 }
 
+// static login method
+
+userSchema.statics.login = async function(email, password) {
+   
+    if(!email || !password){
+        throw Error('All fields must be filled');
+    }
+
+    const user = await this.findOne({email});
+    
+    if(!user){
+        throw Error('Incorrect Email');
+    }
+    const match = await bcrypt.compare(password, user.password);
+
+    if(!match){
+        throw Error('Incorrect Password');
+    }
+
+    return user;
+}
+
 export default mongoose.model('User', userSchema);
